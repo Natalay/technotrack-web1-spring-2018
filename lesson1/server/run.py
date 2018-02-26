8b59202ec866440dd33f569c13839e23387a3116
Sorokina Natalia <sorokinan86@outlook.com>
2018-02-27 02:51:39 +0300

    found path

--- lesson1/server/run.py
+++ lesson1/server/run.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
-import socket
+import socket, sys
 
 
 def get_response(request):
+    copy = request
+    n1 = copy.find("User-Agent")
+    s1 = copy[n1:]
+    m1 = s1.find("\n")
+    return "Hello mister!\nYou are: " + s1[0:m1] + request_string
 
-    return 'you are is...\n'
-
+def get_path(request):
+    n = request.find("GET ")
+    s = request[n+4:] 
+    m = s.find(" ")  
+    path = s[0:m] 
+    return s[0:m]
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -18,8 +27,10 @@ while 1:
     try:
         (client_socket, address) = server_socket.accept()
         print 'Got new client', client_socket.getsockname()  #to printing client`s ip and port 
-        request_string = client_socket.recv(2048)  #reading 2048 bytes from socket 
-        client_socket.send(get_response(request_string))  #messege client some request
+        request_string = client_socket.recv(2048)  #reading 2048 bytes from socket  
+        client_socket.send(get_response(request_string))  #messege to client some request
+	client_socket.send(get_path(request_string))
+	
         client_socket.close()
     except KeyboardInterrupt:  #
         print 'Stopped'
